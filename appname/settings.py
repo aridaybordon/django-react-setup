196a732e87e2141439969fd75cedda9ea62c0e1e
Aridai Bordón <aridaybordon@gmail.com>
2022-07-30 11:59:33 +0100

    update: add ui app to settings

--- appname/settings.py
+++ appname/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
+    'ui.apps.UiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
